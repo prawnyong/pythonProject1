29bdafb34c7b18463089f824ba8d385a3acee76a
Arica <aricaang0395@gmail.com>
2020-11-25 11:36:08 +0800

    v1.0 arica

---
+++ Lab6.py
@@ -0,0 +1,20 @@
+class Vehicle:
+    def init(self, number_of_wheels, type_of_tank, seating_capacity, maximum_velocity):
+        self.number_of_wheels = number_of_wheels
+        self.type_of_tank = type_of_tank
+        self.seating_capacity = seating_capacity
+        self.maximum_velocity = maximum_velocity
+
+
+    def drive (self):
+        print("The vehicle is in driving mode now.")
+
+
+
+vios = Vehicle ('4', 'petrol', 5, 180)
+print(vios.number_of_wheels)
+print(vios.type_of_tank)
+print(vios.seating_capacity)
+print(vios.maximum_velocity)
+
+vios.drive()
